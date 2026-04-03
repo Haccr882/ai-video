f6a4c4b749d30acfe4d7ee16460c3f6c8495610c
Haccr882 <rravindrayadav2228@gmail.com>
2026-04-03 08:59:07 +0530

    Create security.py

---
+++ backend/app/core/security.py
@@ -0,0 +1,28 @@
+from passlib.context import CryptContext
+from jose import jwt
+
+from app.core.config import settings
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
+
+
+def hash_password(password: str):
+
+    return pwd_context.hash(password)
+
+
+def verify_password(password: str, hashed: str):
+
+    return pwd_context.verify(password, hashed)
+
+
+def create_token(data: dict):
+
+    return jwt.encode(
+        data,
+        settings.JWT_SECRET,
+        algorithm="HS256"
+    )
